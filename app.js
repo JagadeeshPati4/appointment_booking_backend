34f522cc011a01184a3889e7ba6f04cbb046aba8
jagadeesh pati <jagadeeshpati4@gmail.com>
2025-02-26 12:52:54 +0530

    backend app changes

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ app.use(bodyParser.json());
 
 app.use(
   cors({
-    origin: "http://localhost:5173", // Allow only frontend origin
-    credentials: true, // Allow cookies/auth headers
+    origin: "http://localhost:5173", 
+    credentials: true, 
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     allowedHeaders: "Origin,X-Requested-With,Content-Type,Accept,Authorization",
   })
